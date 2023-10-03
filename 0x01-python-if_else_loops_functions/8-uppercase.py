24781297699334e64c41ffe1a4fe595bc2517091
ilyas <ilyasouagag2011@gmail.com>
2023-10-03 23:06:29 +0100

    du miniscule au majuscule

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    formated_string = ''
+    for i in str:
+        if i >= 'a' and i <= 'z':
+            formated_string += chr(ord(i) - 32)
+        else:
+            formated_string += i
+    return formated_string
