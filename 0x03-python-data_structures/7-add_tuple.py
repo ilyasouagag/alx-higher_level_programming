a1f7257da28f30d7812b8d12ede05cc0119edcbd
ilyas <ilyasouagag2011@gmail.com>
2023-10-07 17:42:28 +0100

    adding two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) == 0 and len(tuple_b) ==0:
-        return ()
-    if len(tuple_a) == 0:
-        return tuple_b[:2]
-    if len(tuple_b) == 0:
-        return tuple_a[:2]
-    if len(tuple_a) == 1:
-        return tuple_a[0] + tuple_b[0], tuple_b[1]
-    if len(tuple_b) == 1:
-        return tuple_a[0] + tuple_b[0], tuple_a[1]
-    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+    a , b = tuple_a[:2] if len(tuple_a) >= 2 else (tuple_a[0] if len(tuple_a) == 1 else 0, 0)
+    c , d = tuple_b[:2] if len(tuple_b) >= 2 else (tuple_b[0] if len(tuple_b) == 1 else 0, 0)
+    return a + c, b + d
 tuple_a = (1, 89)
 tuple_b = (88, 11)
 new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)  # Output: (89, 100)
+print(new_tuple)
 
-print(add_tuple(tuple_a, (1, )))  # Output: (2, 89)
-print(add_tuple(tuple_a, ())) 
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
