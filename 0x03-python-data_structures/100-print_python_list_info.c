63966018694db94aa4d4f91f8081c371d05be18c
ilyas <ilyasouagag2011@gmail.com>
2023-10-08 01:15:31 +0100

    Cpython shows how python is related to C

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,5 +9,5 @@ void print_python_list_info(PyObject *p)
     printf("[*] Size of the Python List = %li\n", len);
     printf("[*] Allocated = %li\n", object->allocated);
     for (i = 0; i < len; i++)
-        printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+        printf("Element %i: %s\n", i, Py_TYPE(object->ob_item[i])->tp_name);
 }
