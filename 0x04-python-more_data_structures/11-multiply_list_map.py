750e491b757f1f0bdde8ac607734c63c9e96b183
ilyas <ilyasouagag2011@gmail.com>
2023-10-11 16:51:52 +0100

    using map and lambda

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    copy_list = my_list[:]
-    return list(map(lambda x: x * number, copy_list))
+    return list(map(lambda x: x * number, my_list[:]))
