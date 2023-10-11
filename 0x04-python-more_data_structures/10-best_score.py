bc4ea7d95628d76c3c9760e3911d4f9b28d33b3c
ilyas <ilyasouagag2011@gmail.com>
2023-10-11 16:46:41 +0100

    return the key of maximum value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    max = 0
+    if not a_dictionary:
+        return None
+    biggest_key = None
+    for key in a_dictionary:
+        if a_dictionary[key] > max:
+            biggest_key = key
+            max = a_dictionary[key]
+    return biggest_key
