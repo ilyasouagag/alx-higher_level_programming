9c24df79de5ce2d809065dbf15c6123140045ada
ilyas <ilyasouagag2011@gmail.com>
2023-10-11 14:14:54 +0100

    modify values

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for value in my_list:
+        if value == search:
+            new_list.append(replace)
+        else:
+            new_list.append(value)
+    return new_list
+my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+new_list = search_replace(my_list, 2, 89)
+
+print(new_list)
+print(my_list)
