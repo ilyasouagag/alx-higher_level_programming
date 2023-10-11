2c5a766b36993f7edd1d25ed18daa4ea124b2b84
ilyas <ilyasouagag2011@gmail.com>
2023-10-11 17:20:01 +0100

    romanian to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string or not isinstance(roman_string, str):
-        return None
+        return 0
     total = 0
     prev = 0
     dictio = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
