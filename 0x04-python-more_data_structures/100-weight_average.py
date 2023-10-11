16c70521d30e8fdd05843117e196b082a57231a2
ilyas <ilyasouagag2011@gmail.com>
2023-10-11 17:33:21 +0100

    avrage of a list that contains tuples

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    sum = 0
+    mul = 1
+    for row in my_list:
+        for i in row:
+            mul *= i
+        sum += mul
+        mul = 1
+    print(sum)
+    sum2 = 0
+    for row in my_list:
+        sum2 += row[-1]
+    print(sum2)
+    return sum / sum2
