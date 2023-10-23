a5c73f105d1c9eccdad0597c2d5d5cccb4a2278a
ilyas <ilyasouagag2011@gmail.com>
2023-10-23 11:52:21 +0100

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in my_list:
+        x -= 1
         try:
             print("{:d}".format(i), end='')
             c += 1
-            x -= 1
         except (TypeError, ValueError):
             pass
         if x == 0:
