caddc273e706e594f161d5cb9386160f429e29d1
ilyas <ilyasouagag2011@gmail.com>
2023-10-23 12:34:28 +0100

    print  with error exc_info

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from sys import stderr, exc_info
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        print("Exception: {}".format(exc_info()[1]), file=stderr)
+        return False
