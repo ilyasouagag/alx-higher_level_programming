e08af93a84ecb7c9d2692c7e7189306639a7b319
ilyas <ilyasouagag2011@gmail.com>
2023-10-23 11:44:50 +0100

    TypeError ValueError

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,14 +2,25 @@
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
     i = 0
-    try:
-        while (x > 0):
-            if (isinstance(my_list[i], int)):
-                print("{:d}".format(my_list[i]), end='')
-                x -= 1
-                c += 1
-            i += 1
-    except IndexError:
-        pass
+    for  i in my_list:
+        try:
+            print("{:d}".format(i), end='')
+            c += 1
+            x -= 1
+        except (TypeError, ValueError):
+            pass
+        if x == 0:
+            break
     print()
     return c
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
