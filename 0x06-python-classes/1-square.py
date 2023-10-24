79534e6d70845a1107768e4a70310548f46a3c16
ilyas <ilyasouagag2011@gmail.com>
2023-10-24 12:16:00 +0100

    private attributes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""this is the second task of the project"""
+
+
+class Square:
+    """empty class that defines a square"""
+
+    def __init__(self, size):
+        """private instance attribute"""
+        self.__size = size
