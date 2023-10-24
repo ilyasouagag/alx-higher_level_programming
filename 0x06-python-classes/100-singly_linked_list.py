95c424b3b7a574ce66ed530b1d83cf80ec1c74df
ilyas <ilyasouagag2011@gmail.com>
2023-10-24 16:55:50 +0100

    linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -49,21 +49,19 @@ class SinglyLinkedList:
     def sorted_insert(self, value):
         """sort elements by data in increasing order"""
         new = Node(value)
-        current = self.__head
-        prev = None
         if self.__head is None:
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
             self.__head = new
         else:
-            while current is not None:
-                if value < current.data:
-                    break
-                prev = current
+            current = self.__head
+            while (current.next_node is not None and
+                    current.next_node.data < value):
                 current = current.next_node
-            new.next_node = current
-            if prev is None:
-                self.__head = new
-            else:
-                prev.next_node = new
+            new.next_node = current.next_node
+            current.next_node = new
 
     def __str__(self):
         """print elements of list followed by a new line"""
