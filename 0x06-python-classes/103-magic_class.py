5e09f20bf1e94afdbc4349c7dfe1fa6649f20231
ilyas <ilyasouagag2011@gmail.com>
2023-10-24 17:59:43 +0100

    disassembling a class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,12 +11,12 @@ class MagicClass:
         """all attributes of object of MagicClass"""
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
-        self._MagicClass__radius = radius
+        self.__radius = radius
 
     def area(self):
         """return the area of MagicClass"""
-        return self._MagicClass__radius ** 2 * math.pi
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         """return the circumference of the magicClass"""
-        return 2 * math.pi * self._MagicClass__radius
+        return 2 * math.pi * self.__radius
