d8c66f593b741231b8993b393ae931e4c5fceef8
ilyas <ilyasouagag2011@gmail.com>
2023-10-24 17:57:21 +0100

    dissasembling a class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+import math
+
+"""magicclass calculate the area and circumferenece of a circle"""
+
+
+class MagicClass:
+    """representation of circle"""
+
+    def __init__(self, radius):
+        """all attributes of object of MagicClass"""
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """return the area of MagicClass"""
+        return self._MagicClass__radius ** 2 * math.pi
+
+    def circumference(self):
+        """return the circumference of the magicClass"""
+        return 2 * math.pi * self._MagicClass__radius
