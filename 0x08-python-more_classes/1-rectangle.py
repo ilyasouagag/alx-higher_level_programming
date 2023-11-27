ac28211e808c75ca12b528777d697d3139a51b7a
ilyasouagag <ilyasouagag2011@gmail.com>
2023-11-27 17:32:48 +0100

    class rectangle with private attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """private instance attribute and raise exceptions"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
