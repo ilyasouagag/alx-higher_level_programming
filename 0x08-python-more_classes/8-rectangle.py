8b9003dcb7ca9da1bcc2bb2323526c19bd814150
ilyasouagag <ilyasouagag2011@gmail.com>
2023-11-27 19:48:32 +0100

    classmethod and staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,6 +74,7 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """check whick instance have the bigger area and returns it"""
         if not isinstance(rect_1, Rectangle):
