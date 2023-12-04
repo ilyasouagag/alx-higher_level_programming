2667941d72ec8facf4045d8740c59e1c7c1a7a4f
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 20:42:59 +0100

    my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,13 +5,3 @@ class MyList(list):
 		for i in sorted(self):
 			a.append(i)
 		print(a)
-
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
