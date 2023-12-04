20f7c62571831a7343c09acec6b1918bf204a4e2
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 22:37:25 +0100

    full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
-class BaseGeometry:
-	def area(self):
-		raise Exception("area() is not implemented")
-	def integer_validator(self,name,value):
-		if type(value) is not int:
-			raise TypeError("{} must be an integer".format(name))
-		if value <= 0:
-			raise ValueError("{} must be greater than 0".format(name))
+"""lass Rectangle inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""create class named Rectangle"""
+
+
 class Rectangle(BaseGeometry):
-	def __init__(self,width,height):
-		self.__width = width
-		self.__height = height
-		super().integer_validator('width', self.__width)
-		super().integer_validator('height', self.__height)
+    """class that inherits from basegeometry"""
+
+    def __init__(self, width, height):
+        """initialise atributtes for instances"""
+        self.__width = width
+        self.__height = height
+        super().integer_validator('width', self.__width)
+        super().integer_validator('height', self.__height)
+
+    def area(self):
+        """calculate the area of rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """print a description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
