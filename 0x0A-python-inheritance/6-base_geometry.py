7b9537d77b1c948f4707c48f3c3a747d3bebd1e6
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 21:52:08 +0100

    method in a class

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""create class named BaseGeometry"""
+
+
 class BaseGeometry:
-	def area(self):
-		raise Exception("area() is not implemented")
+    """empty class"""
+
+    def area(self):
+        """area method with a exception"""
+        raise Exception("area() is not implemented")
