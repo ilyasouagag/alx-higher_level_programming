f6dd3b4c4f2a5fe146242c7ce1d444e81bc1d97d
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 20:44:20 +0100

    inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Define a class mylist and function print sorted"""
 class MyList(list):
+	"""Mylist class inhereted list attributes"""
 	def print_sorted(self):
+		"""print sorted method"""
 		a = []
 		for i in sorted(self):
 			a.append(i)
