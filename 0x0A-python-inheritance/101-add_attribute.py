b22349c958ef6adbcefef433021abb1e7f35a222
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 23:10:59 +0100

    hasattr and setattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""function that adds a new attribute to a class"""
+
+
+def add_attribute(obj, attr, value):
+    """dict attribute is a dictionnary that stores
+    the object's attributes"""
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add a new attribute")
+    setattr(obj, attr, value)
