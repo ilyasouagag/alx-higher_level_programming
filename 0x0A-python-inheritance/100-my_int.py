980591d766f2486374fa5d05a48a51eeceb26523
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-04 22:53:00 +0100

    invert == and !=

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""class that inherits from int"""
+
+
+class MyInt(int):
+    """function that invert == and !="""
+
+    def __eq__(self, value):
+        """invert == to !="""
+        return self.real != value
+
+    def __ne__(self, value):
+        """invert != to =="""
+        return self.real == value
