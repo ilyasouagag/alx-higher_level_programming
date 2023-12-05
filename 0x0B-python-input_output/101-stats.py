2f44a339f04fc3a4b49ee45ac4f5cbe3329726e1
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-05 18:28:37 +0100

    last task

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""cript that reads stdin line by line and computes metrics"""
+import sys
+
+size = 0
+possible_status = {"200": 0, "301": 0, "400": 0, "401": 0,
+                   "403": 0, "404": 0, "405": 0, "500": 0}
+i = 0
+try:
+    for line in sys.stdin:
+        args = line.split()
+        if len(args) >= 2:
+            a = i
+            if args[-2] in possible_status:
+                possible_status[args[-2]] += 1
+                i += 1
+            try:
+                size += int(args[-1])
+                if a == i:
+                    i += 1
+            except FileNotFoundError:
+                if a == i:
+                    continue
+        if i % 10 == 0:
+            print("File size: {:d}".format(size))
+            for key, value in sorted(possible_status.items()):
+                if value:
+                    print("{:s}: {:d}".format(key, value))
+    print("File size: {:d}".format(size))
+    for key, value in sorted(possible_status.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
+
+except KeyboardInterrupt:
+    print("File size: {:d}".format(size))
+    for key, value in sorted(possible_status.items()):
+        if value:
+            print("{:s}: {:d}".format(key, value))
