2b95a2517ac9d615d57f3b48b7108084757b95a9
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-05 07:43:47 +0100

    3 tasks write append and dumps

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that writess a text file and prints it to stdout"""
+
+
+def write_file(filename="", text=""):
+    """Write a function that writes a text file and prints it to stdout"""
+    with open(filename, 'w', encoding='UTF-8') as file:
+        return file.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""Write a function that appends a text file and prints it to stdout"""
+
+
+def append_write(filename="", text=""):
+    """Write a function that appends a text file and prints it to stdout"""
+    with open(filename, 'a', encoding='UTF-8') as file:
+        return file.write(text)
