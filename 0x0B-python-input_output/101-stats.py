ac1d73c8133b55a976951d19f64f2cc8f6761ae4
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-05 18:29:39 +0100

    last task

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,16 +3,16 @@
 import sys
 
 size = 0
-possible_status = {"200": 0, "301": 0, "400": 0, "401": 0,
-                   "403": 0, "404": 0, "405": 0, "500": 0}
+status = {"200": 0, "301": 0, "400": 0, "401": 0,
+          "403": 0, "404": 0, "405": 0, "500": 0}
 i = 0
 try:
     for line in sys.stdin:
         args = line.split()
         if len(args) >= 2:
             a = i
-            if args[-2] in possible_status:
-                possible_status[args[-2]] += 1
+            if args[-2] in status:
+                status[args[-2]] += 1
                 i += 1
             try:
                 size += int(args[-1])
@@ -23,16 +23,16 @@ try:
                     continue
         if i % 10 == 0:
             print("File size: {:d}".format(size))
-            for key, value in sorted(possible_status.items()):
+            for key, value in sorted(status.items()):
                 if value:
                     print("{:s}: {:d}".format(key, value))
     print("File size: {:d}".format(size))
-    for key, value in sorted(possible_status.items()):
+    for key, value in sorted(status.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
 
 except KeyboardInterrupt:
     print("File size: {:d}".format(size))
-    for key, value in sorted(possible_status.items()):
+    for key, value in sorted(status.items()):
         if value:
             print("{:s}: {:d}".format(key, value))
