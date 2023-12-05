1c9b3a1ef5bc399b2f86f8679cd3a94c9568e98f
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-05 07:24:27 +0100

    read  file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read a text file and prints it"""
+
+
+def read_file(filename=""):
+    """print it to stdout"""
+    with open(filename, 'r', encoding='UTF-8') as file:
+        print(file.read())
