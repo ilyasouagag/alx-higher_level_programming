15dcf042acdee2dae8959e63056b9b14526a58c4
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-05 18:24:02 +0100

    writelines

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,15 +3,15 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """this function uses writelines that takes from a list"""
-    with open(filename, 'r', encoding='UTF-8') as file:
+    """writelines take a list of strings as parameter"""
+    with open(filename, 'r', encoding='utf-8') as file:
         final_string = []
         while True:
-            for line in file:
-                if line == "":
-                    break
-                final_string.append(line)
-                if search_string in line:
-                    final_string.append(search_string)
-    with open(filename, 'w', encoding='UTF-8') as file:
+            line = file.readline()
+            if line == "":
+                break
+            final_string.append(line)
+            if search_string in line:
+                final_string.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as file:
         file.writelines(final_string)
