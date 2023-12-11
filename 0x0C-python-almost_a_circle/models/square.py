be459932e0aaf13b6a1796151f3d1c23786b2516
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-11 18:28:38 +0100

    last update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     """iherits from Rectangle"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """initialise instances for Square"""
         super().__init__(size, size, x, y, id)
 
     @property
