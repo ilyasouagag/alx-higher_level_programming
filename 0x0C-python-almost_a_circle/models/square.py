9c148203386962e62613df654852bffcf53b7c78
ilyasouagag <ilyasouagag2011@gmail.com>
2023-12-11 17:51:47 +0100

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,8 @@ class Square(Rectangle):
 
     def __str__(self):
         """return a representation"""
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+        return "[Square] ({}) {}/{} - {}".\
+            format(self.id, self.x, self.y, self.width)
 
     def update(self, *args, **kwargs):
         """update the class"""
