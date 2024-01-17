3d5c4172b4598432caf69d2a229201838127af13
ilyasouagag <ilyasouagag2011@gmail.com>
2024-01-17 22:16:11 +0100

    LEFT JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,5 @@
 -- ists all cities contained in the database hbtn_0d_usa.
-SELECT id,name
+SELECT cities.id,cities.name,states.name
 FROM cities
-RIGHT JOIN states ON cities.state_id = states.id;
+LEFT JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
