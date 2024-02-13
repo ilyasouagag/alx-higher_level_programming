291c55c9796f2d99322ca9d54c6fe5ba1e43a2c7
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 23:22:47 +0100

    inheritnig from square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const SquareP = require('./5-square.js');
+class Square extends SquareP {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      console.log('');
+    }
+  }
+}
+module.exports = Square;
