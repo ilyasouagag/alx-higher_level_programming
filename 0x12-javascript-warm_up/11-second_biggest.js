61b1dae777cef9297d873a5181c064a10f7580d1
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 03:48:21 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@ if (process.argv[2] === undefined || process.argv.length === 3) {
   console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
-  const secondMax = arr.sort(function (a, b) { return b - a; })[1];
+  const sortedArr = arr.sort(function (a, b) { return b - a; });
+  const secondMax = sortedArr[1];
   console.log(secondMax);
 }
