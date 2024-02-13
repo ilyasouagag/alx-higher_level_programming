70ce1b4470f759cd031a99e47c4a60bcc39f50fb
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 03:10:45 +0100

    factorial recursivly

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+function fact (a) {
+  if (a === 0) {
+    return 1;
+  }
+  return a * fact(a - 1);
+}
+if (process.argv[2] === undefined) {
+  console.log('1');
+} else {
+  console.log(fact(parseInt(process.argv[2])));
+}
