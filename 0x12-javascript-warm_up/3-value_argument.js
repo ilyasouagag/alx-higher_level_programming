6f4ffb65fe339e169c48ae07e008838013560882
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 01:05:25 +0100

    print argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const args = process.argv.length - 2;
-if (args === 0) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
