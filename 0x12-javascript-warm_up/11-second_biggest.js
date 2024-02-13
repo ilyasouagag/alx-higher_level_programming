e9bc04a8d6e889d2dd196274ce8742f5f2fc75fc
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 03:18:16 +0100

    maximum

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+if (process.argv[2] === undefined || process.argv.length === 3) {
+  console.log('0');
+} else {
+  let max = 0;
+  for (let i = 3; i < process.argv.length - 2; i++) {
+    max = process.argv[2];
+    if (process.argv[i] > max) {
+      max = process.argv[i];
+    }
+  }
+  console.log(max);
+}
