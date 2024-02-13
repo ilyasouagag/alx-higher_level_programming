fb6969e9da0ef5f99094338fdbc8808dbf9ae932
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-13 01:39:14 +0100

    nested loops

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+}
+for (let i = 0; i < process.argv[2]; i++) {
+  for (let j = 0; j < process.argv[2]; j++) {
+    process.stdout.write('X');
+  }
+  console.log('');
+}
