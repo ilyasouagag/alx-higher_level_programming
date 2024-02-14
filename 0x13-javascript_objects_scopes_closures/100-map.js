fc96752784d5c9b99cfcde00948e82003f3dcaf5
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-14 03:26:31 +0100

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const newList = list.map((x) => x * list.indexOf(x));
+const newList = list.map((item, index) => item * index);
 console.log(newList);
