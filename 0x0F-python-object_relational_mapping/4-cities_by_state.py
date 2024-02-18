760bd612a679494db93ae4963282afd1ef903abd
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 00:14:10 +0100

    using jointures in query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], database=argv[3], port=3306)
+    cu = db.cursor()
+    cu.execute(
+        "SELECT cities.id,cities.name,states.name FROM cities LEFT JOIN states on states.id = cities.state_id ")
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
+    cu.close()
+    db.close()
