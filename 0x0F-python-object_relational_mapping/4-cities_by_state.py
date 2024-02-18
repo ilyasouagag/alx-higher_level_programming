54f41373fdad161cf1c2f463cb6cec5cf92765bd
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 00:17:35 +0100

    using jointures

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         host="localhost", user=argv[1],
         passwd=argv[2], database=argv[3], port=3306)
     cu = db.cursor()
-    cu.execute(
-        "SELECT cities.id,cities.name,states.name FROM cities LEFT JOIN states on states.id = cities.state_id ")
+    cu.execute("""SELECT cities.id,cities.name,states.name
+               FROM cities LEFT JOIN states on states.id = cities.state_id""")
     rows = cu.fetchall()
     for row in rows:
         print(row)
