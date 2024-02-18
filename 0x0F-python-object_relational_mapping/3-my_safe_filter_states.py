e6c0b7ed7fa532261287d1f04c5c2d97c0d61ca2
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-18 23:59:46 +0100

    safe filter using %s

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], database=argv[3], port=3306)
+    cu = db.cursor()
+    cu.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY %s"
+        "ORDER BY states.id", (argv[4],))
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
+    cu.close()
+    db.close()
