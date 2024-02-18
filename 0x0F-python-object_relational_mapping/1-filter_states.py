880bc37b36f98dfb25990d2af2534d84f34880cc
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-18 23:22:23 +0100

    filter, same as before, just changing the query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""lists all states from a with a name starting
+with N database as argument"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], database=argv[3], port=3306)
+    cu = db.cursor()
+    cu.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    rows = cu.fetchall()
+    for row in rows:
+        print(row)
+    cu.close()
+    db.close()
