1b5bc9c026a28cde235dc70c5418a8f7046fadfa
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-18 23:45:41 +0100

    passing an argument in the query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         passwd=argv[2], database=argv[3], port=3306)
     cu = db.cursor()
     cu.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id", (argv[4],))
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id".format(argv[4]))
     rows = cu.fetchall()
     for row in rows:
         print(row)
