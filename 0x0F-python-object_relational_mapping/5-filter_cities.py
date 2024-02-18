41ec69a46ba590c57a209512136965070bf6f3ef
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 00:38:36 +0100

    using * and sep to print a list separated by ", "

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+""" lists all cities from the database hbtn_0e_4_usa"""
+import MySQLdb
+from sys import argv
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", user=argv[1],
+        passwd=argv[2], database=argv[3], port=3306)
+    cu = db.cursor()
+    cu.execute("""SELECT cities.name
+               FROM cities LEFT JOIN states on
+               states.id = cities.state_id where
+               states.name LIKE %s""", (argv[4],))
+    rows = cu.fetchall()
+    list_to_print = []
+    for row in rows:
+        list_to_print.append(row[0])
+    print(*list_to_print, sep=", ")
+    cu.close()
+    db.close()
