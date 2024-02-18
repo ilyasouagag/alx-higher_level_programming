76a64b99110553d4b8e94acf4670e3d445018d33
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-18 23:49:17 +0100

    passing an argument in query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 from sys import argv
 if __name__ == "__main__":
@@ -8,7 +9,9 @@ if __name__ == "__main__":
         passwd=argv[2], database=argv[3], port=3306)
     cu = db.cursor()
     cu.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id".format(argv[4]))
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+        "ORDER BY states.id".format(argv[4])
+    )
     rows = cu.fetchall()
     for row in rows:
         print(row)
