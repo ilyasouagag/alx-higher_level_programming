885e08e68edc1a40246b078ecaa972e0a9eb1e5c
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 02:43:30 +0100

    using jointures in sqlalchemy

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""ython file that contains the class definition of a State"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """conatains id and name and id of states of each city"""
+    __tablename__ = "cities"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
