4301ad7eb336bb367fe17c32283f4c82014837d8
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 01:00:11 +0100

    first table using sqlalchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,8 +3,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
+
+
 class State(Base):
     """conatains id and name of each state"""
     __tablename__ = "states"
-    id = Column(Integer,unique=True,nullable=False,primary_key=True)
-    name = Column(String(128),nullable=False)
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
