e651a2e7359b37ca861692ff02c72fc93d38b37c
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 02:58:11 +0100

    advanced tasks

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""ython file that contains the class definition of a State"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+Base = declarative_base()
+
+
+class State(Base):
+    """conatains id and name of each state"""
+    __tablename__ = "states"
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="states")
