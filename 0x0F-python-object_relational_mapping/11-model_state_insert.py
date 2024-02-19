f372f80f1386fab56fefb36e6354fefbc3786580
ilyasouagag <ilyasouagag2011@gmail.com>
2024-02-19 02:25:37 +0100

    update

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    object = State(name="louisiana")
+    session.add(object)
+    element = session.query(State).filter_by(name='Louisiana').first()
+    print(element.id)
+    session.commit()
--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sys import argv
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    element = session.query(State).filter_by(id=2).first()
+    element.name = "New Mexico"
+    session.commit()
