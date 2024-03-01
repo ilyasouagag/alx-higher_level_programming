8229d405b2399499ab1cba99895bc36e9d1d57df
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 00:47:36 +0100

    error code using requests

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request
+to the URL and displays the body of the response.
+using requests module
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
