9b58e8bb69f16d9e3bd202ab8fddc4b2c3d03b96
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 00:04:22 +0100

    post request and display the body

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL and an email,
+sends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response
+(decoded in utf-8)
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import urllib.request
+    import urllib.parse
+    data = {'email': argv[2]}
+    data_url = urllib.parse.urlencode(data).encode('ascii')
+    request = urllib.request.Request(argv[1], data_url)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode('utf-8'))
