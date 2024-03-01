a35f135382d4d33b5083878b416e2976106799ef
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-01 23:20:40 +0100

    using dict and get

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+a Python script that takes in a URL, sends a request
+to the URL and displays the value of the X-Request-Id
+variable found in the header of the response.
+"""
+if __name__ == '__main__':
+    from sys import argv
+    import urllib.request
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(dict(response.headers).get('X-Request-Id'))
