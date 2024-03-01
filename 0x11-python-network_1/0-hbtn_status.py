4bd4a6bca3e0cc2231766072c8621ee479339fc5
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-01 23:00:20 +0100

    get a response from a link

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,7 +6,7 @@ module. The script makes a GET request to the URL
 "https://alx-intranet.hbtn.io/status" and prints
 information about the response.
 """
-if __name__ == "__main__":
+if __name__ == '__main__':
     import urllib.request
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as link:
         response = link.read()
