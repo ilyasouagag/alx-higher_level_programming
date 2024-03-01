971d3f117b8500c455fa1318658467cdb7fd4aa5
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-01 23:05:07 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
 """
-This Python script demonstrates how to fetch
-the content of a URL using the urllib.request
-module. The script makes a GET request to the URL
-"https://alx-intranet.hbtn.io/status" and prints
-information about the response.
-"""
+
+
 if __name__ == '__main__':
     import urllib.request
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as link:
-        response = link.read()
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
         print("Body response:")
-        print(f"\t- type: {type(response)}")
-        print(f"\t- content: {response}")
-        print(f"\t- utf8 content: {response.decode("utf-8")}")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
