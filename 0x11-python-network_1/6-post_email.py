d05322a342a5cf99a4626cfd742e1f5ae7ec9f66
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 00:42:03 +0100

    post an email using requests module

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,6 +8,6 @@ using module requests
 if __name__ == "__main__":
     import requests
     from sys import argv
-    data = {"email" : argv[2]}
-    post = requests.post(argv[1],data)
+    data = {"email": argv[2]}
+    post = requests.post(argv[1], data)
     print(post.text)
