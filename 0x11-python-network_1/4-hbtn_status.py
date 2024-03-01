4d2c4ce7439a960ac07a56816afe1e668e349951
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 00:27:06 +0100

    using requests module

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Python script that fetches https://alx-intranet.hbtn.io/status
+using requests module
+"""
+if __name__ == "__main__":
+    import requests
+    req = requests.get("https://alx-intranet.hbtn.io/status")
+    response = req.text
+    print("Body response:")
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
