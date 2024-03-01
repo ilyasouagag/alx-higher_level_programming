ca2a9318502dd7dfb60f22d765a8beb5b9b79f66
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 00:41:13 +0100

    post an email using module requests

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as
+a parameter, and finally displays the body of the response.
+using module requests
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    data = {"email" : argv[2]}
+    post = requests.post(argv[1],data)
+    print(post.text)
