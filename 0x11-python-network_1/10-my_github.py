082d94e394aa3633764397834795ae77fbcc58c6
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 01:20:50 +0100

    How to use Github's API

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,5 +10,4 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json())
     print(r.json().get("id"))
