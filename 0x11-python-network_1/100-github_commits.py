8efc834a1bb9507de200a43ef6001b03b3e1a190
ilyasouagag <ilyasouagag2011@gmail.com>
2024-03-02 01:29:04 +0100

    last 10 commits in a repo of github using API

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
+"""
+
+
+if __name__ == "__main__":
+    import sys
+    import requests
+    link = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(link)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
